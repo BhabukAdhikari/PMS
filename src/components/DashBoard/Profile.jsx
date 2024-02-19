d159921c034a0cc5ea2c39d54749323be6ad92bc
Bk10ST <133320594+Bk10ST@users.noreply.github.com>
2024-02-18 22:57:46 -0800

    profile responsive ness update

--- src/components/DashBoard/Profile.jsx
+++ src/components/DashBoard/Profile.jsx
@@ -7,34 +7,27 @@ import Calendar from "./Calender";
 const Profile = () => {
   const [TotalTask, setTotalTask] = useState(10);
   const data = [
-    ['week', 'task'],
-    ['Sunday', TotalTask],
-    ['Monday', 55],
-    ['Tuesday', 100],
-    ['Wednesday', 44],
-    ['Thursday', 24],
-    ['Friday', 15] ,
-    ['Saturday', 15]
+    ["week", "task"],
+    ["Sunday", TotalTask],
+    ["Monday", 55],
+    ["Tuesday", 100],
+    ["Wednesday", 44],
+    ["Thursday", 24],
+    ["Friday", 15],
+    ["Saturday", 15],
   ];
 
   const options = {
-    title: 'Your Weekly Analysis',
-    chartArea: { width: '80%', height: '80%'  },
-    hAxis: { title: 'week', minValue: 0 },
-    vAxis: { title: 'task bar' },
-    colors: ["7B61FF"]
+    title: "Your Weekly Analysis",
+    chartArea: { width: "80%", height: "80%", backgroundColor: "e3e7e6" },
+    hAxis: { title: "week", minValue: 0 },
+    vAxis: { title: "task bar" },
+    backgroundColor: "#e3e7e6",
+    colors: ["7B61FF"],
   };
 
   return (
-    <div
-      style={{
-        backgroundColor: "#e3e7e6",
-        marginTop: "0%",
-        color: "black",
-        padding: "5%",
-        marginLeft: "14.6%",
-      }}
-    >
+    <div className="profile-department">
       <div className="card" style={{ width: "18rem" }} id="profle-box">
         <img src={User} className="card-img-top" alt="..." id="profile-img" />
         <div className="card-body" id="profile-box-text">
@@ -49,92 +42,144 @@ const Profile = () => {
 
       <div className="task-progress-container">
         <div
-          className="card text-white mb-3"
-          style={{ maxWidth: "18rem" }}
-          id="task-box"
+          className="card"
+          id="task-Details"
+          style={{
+            width: "18rem",
+            height: "9rem",
+            borderRadius: "15px",
+            backgroundColor: "#7B61FF",
+            color: "white",
+          }}
         >
-          <div className="card-header">Task Box</div>
-          <div className="card-body" id="profile-text">
-            <i
-              class="fa-solid fa-list-check"
+          <div className="card-body">
+            <h5 className="card-title">{TotalTask}</h5>
+            <p
+              className="card-text"
               style={{
                 color: "white",
-                paddingRight: "5%",
-                paddingTop: "1%",
+                fontWeight: "bold" 
               }}
-            ></i>
-            <h5 className="card-title" id="sub-title">
-              No. of Task
-            </h5>
-            <p className="card-text" id="number-display">
-              {TotalTask}
+            >
+              List Of task
             </p>
+            <a
+              href="#"
+              className="btn"
+              style={{
+                color: "white",
+                border: "2px solid white",
+                width: "30%", 
+                height: "30%" , 
+                fontSize: "90%",
+                paddingBottom: "5%"
+              }}
+              id="view"
+            >
+              View
+            </a>
           </div>
         </div>
+
         <div
-          className="card text-white mb-3"
-          style={{ maxWidth: "18rem" }}
-          id="task-box"
+          className="card"
+          id="task-Details"
+          style={{
+            width: "18rem",
+            height: "9rem",
+            borderRadius: "15px",
+            backgroundColor: "#7B61FF",
+            color: "white",
+          }}
         >
-          <div className="card-header">Task Complete Box</div>
-          <div className="card-body" id="profile-text">
-            <i
-              class="fa-solid fa-list-check"
+          <div className="card-body">
+            <h5 className="card-title">{TotalTask}</h5>
+            <p
+              className="card-text"
               style={{
                 color: "white",
-                paddingRight: "5%",
-                paddingTop: "1%",
+                fontWeight: "bold" 
               }}
-            ></i>
-            <h5 className="card-title" id="sub-title">
-              Complete
-            </h5>
-            <p className="card-text" id="number-display">
-              {TotalTask}
+            >
+              List Of task
             </p>
+            <a
+              href="#"
+              className="btn"
+              style={{
+                color: "white",
+                border: "2px solid white",
+                width: "30%", 
+                height: "30%" , 
+                fontSize: "90%",
+                paddingBottom: "5%"
+              }}
+              id="view"
+            >
+              View
+            </a>
           </div>
         </div>
+
+
         <div
-          className="card text-white mb-3"
-          style={{ maxWidth: "18rem" }}
-          id="task-box"
+          className="card"
+          id="task-Details"
+          style={{
+            width: "18rem",
+            height: "9rem",
+            borderRadius: "15px",
+            backgroundColor: "#7B61FF",
+            color: "white",
+          }}
         >
-          <div className="card-header">Task Pending Box</div>
-          <div className="card-body" id="profile-text">
-            <i
-              class="fa-solid fa-list-check"
+          <div className="card-body">
+            <h5 className="card-title">{TotalTask}</h5>
+            <p
+              className="card-text"
               style={{
                 color: "white",
-                paddingRight: "5%",
-                paddingTop: "1%",
+                fontWeight: "bold" 
               }}
-            ></i>
-            <h5 className="card-title" id="sub-title">
-              Pending
-            </h5>
-            <p className="card-text" id="number-display">
-              {TotalTask}
+            >
+              List Of task
             </p>
+            <a
+              href="#"
+              className="btn"
+              style={{
+                color: "white",
+                border: "2px solid white",
+                width: "30%", 
+                height: "30%" , 
+                fontSize: "90%",
+                paddingBottom: "5%"
+              }}
+              id="view"
+            >
+              View
+            </a>
           </div>
         </div>
+
       </div>
 
-    <div style={{ width: '100%', maxWidth: 600, height: 500  }} className="task-graph">
-      <Chart
-        chartType="BarChart"
-        data={data}
-        options={options}
-        width={'100%'}
-        height={'100%'}
-      />
-    </div>
- 
-<div className="calender-section">
-  <Calendar/>
-</div>
+      <div
+        style={{ width: "100%", maxWidth: 600, height: 500 }}
+        className="task-graph"
+      >
+        <Chart
+          chartType="BarChart"
+          data={data}
+          options={options}
+          width={"100%"}
+          height={"100%"}
+        />
+      </div>
 
-
-    
+      <div className="calender-section">
+        <Calendar />
+      </div>
     </div>
   );
 };
